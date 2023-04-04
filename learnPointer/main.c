1b262bdcc8e321f73510609442b25cb4dd8e645a
sunao <dustasa@icloud.com>
2023-04-04 16:39:05 +0800

    typecasting

--- learnPointer/main.c
+++ learnPointer/main.c
@@ -10,14 +10,17 @@
 
 int main() {
     int a = 1025;
+    // 1025 = 00000000 00000000 00000100 00000001
     int* p;
     p = &a;
     printf("size of integer is %d \n", sizeof(int));
     printf("Address of p is %d, value = %d \n", p, *p);
+    printf("Address of p+1 is %d, value = %d \n", p+1, *(p+1));
     
     char* p0;
-    p0 = (char*)p;
+    p0 = (char*)p; // typecasting
     printf("size of char is %d \n", sizeof(char));
-    printf("Address of p is %d, value = %d \n", p0, *p0);
+    printf("Address of p0 is %d, value = %d \n", p0, *p0);
+    printf("Address of p0+1 is %d, value = %d \n", p0+1, *(p0+1));
     
 }
